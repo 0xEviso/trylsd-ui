a6e24818e3f19483e53b3f4b630b596fef4f8852
Eviso <eviso@protonmail.com>
2023-12-03 12:39:30 +0700

    hotfix

--- src/components/SwapForm.tsx
+++ src/components/SwapForm.tsx
@@ -47,7 +47,7 @@ export default function SwapForm() {
                   id="tabs"
                   name="tabs"
                   className="block w-full rounded-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500"
-                  defaultValue={tabs.find((tab) => tab.current).name}
+                  defaultValue={tabs.find((tab) => tab.current)?.name}
                 >
                   {tabs.map((tab) => (
                     <option key={tab.name}>{tab.name}</option>
