488fa05c56869768051e185369f0cab2f638b327
Eviso <eviso@protonmail.com>
2023-12-12 10:39:42 +0700

    improve landing page ux

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import { ArrowPathIcon, ServerStackIcon, LockClosedIcon, ChartBarIcon, BanknotesIcon } from '@heroicons/react/20/solid'
-
+import Link from 'next/link';
 
 const features = [
   {
@@ -28,8 +28,8 @@ const features = [
 export default function Home() {
   return (
     <>
-      <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-        <div className="hidden sm:mb-8 sm:flex sm:justify-center">
+      <div className="mx-auto max-w-2xl py-30 sm:py-35 lg:py-40">
+        {/* <div className="hidden sm:mb-8 sm:flex sm:justify-center">
           <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-400 ring-1 ring-white/10 hover:ring-white/20">
             Announcing our next round of funding.{' '}
             <a href="#" className="font-semibold text-white">
@@ -37,24 +37,24 @@ export default function Home() {
               Read more <span aria-hidden="true">&rarr;</span>
             </a>
           </div>
-        </div>
+        </div> */}
         <div className="text-center">
           <h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
-            Maximize your $ETH yields
+            Maximize your yield on ETH
           </h1>
           <p className="mt-6 text-lg leading-8 text-gray-300">
             YieldNest TryLSD pool allows you to generate up to 8% APY on your
           </p>
           <div className="mt-10 flex items-center justify-center gap-x-6">
-            <a
-              href="#"
+            <Link
+              href={'/deposit'}
               className="rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400"
             >
               Get started
-            </a>
-            <a href="#" className="text-sm font-semibold leading-6 text-white">
+            </Link>
+            <Link href={'/deposit#how-it-works'} className="text-sm font-semibold leading-6 text-white">
               Learn more <span aria-hidden="true">→</span>
-            </a>
+            </Link>
           </div>
         </div>
       </div>
@@ -87,12 +87,12 @@ export default function Home() {
       <div className="mx-auto max-w-2xl py-16 sm:py-24 lg:py-28">
         <div className="text-center">
           <div className="mt-10 flex items-center justify-center gap-x-6">
-            <a
-              href="#"
+            <Link
+              href={'/deposit'}
               className="rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400"
             >
               Go to Deposit Page
-            </a>
+            </Link>
           </div>
         </div>
       </div>
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -32,52 +32,50 @@ export default function Header() {
         {({ open }) => (
           <>
             <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
-              <div className="border-b border-gray-700">
-                <div className="flex h-16 items-center justify-between px-4 sm:px-0">
-                  <div className="flex items-center">
-                    <div className="flex-shrink-0">
-                      <Image
-                        className="h-8 w-32"
-                        alt="YieldNest logo"
-                        src={YieldNestLogo} />
-                    </div>
-                    <div className="hidden md:block">
-                      <div className="ml-10 flex items-baseline space-x-4">
-                        {navigation.map((item) => (
-                          <Link
-                            key={item.name}
-                            href={item.href}
-                            className={classNames(
-                              (currentPathname == item.href)
-                                ? 'bg-gray-900 text-white'
-                                : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                              'rounded-md px-3 py-2 text-sm font-medium'
-                            )}
-                            aria-current={(currentPathname == item.href) ? 'page' : undefined}
-                          >
-                            {item.name}
-                          </Link>
-                        ))}
-                      </div>
-                    </div>
+              <div className="flex h-16 items-center justify-between px-4 sm:px-0">
+                <div className="flex items-center">
+                  <div className="flex-shrink-0">
+                    <Image
+                      className="h-8 w-32"
+                      alt="YieldNest logo"
+                      src={YieldNestLogo} />
                   </div>
                   <div className="hidden md:block">
-                    <div className="ml-4 flex items-center md:ml-6 text-base	">
-                      <ConnectButton accountStatus="address" showBalance={false} />
+                    <div className="ml-10 flex items-baseline space-x-4">
+                      {navigation.map((item) => (
+                        <Link
+                          key={item.name}
+                          href={item.href}
+                          className={classNames(
+                            (currentPathname == item.href)
+                              ? 'bg-gray-900 text-white'
+                              : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                            'rounded-md px-3 py-2 text-sm font-medium'
+                          )}
+                          aria-current={(currentPathname == item.href) ? 'page' : undefined}
+                        >
+                          {item.name}
+                        </Link>
+                      ))}
                     </div>
                   </div>
-                  <div className="-mr-2 flex md:hidden">
-                    {/* Mobile menu button */}
-                    <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                      <span className="absolute -inset-0.5" />
-                      <span className="sr-only">Open main menu</span>
-                      {open ? (
-                        <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
-                      ) : (
-                        <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
-                      )}
-                    </Disclosure.Button>
+                </div>
+                <div className="hidden md:block">
+                  <div className="ml-4 flex items-center md:ml-6 text-base	">
+                    <ConnectButton accountStatus="address" showBalance={false} />
                   </div>
+                </div>
+                <div className="-mr-2 flex md:hidden">
+                  {/* Mobile menu button */}
+                  <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                    <span className="absolute -inset-0.5" />
+                    <span className="sr-only">Open main menu</span>
+                    {open ? (
+                      <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                    ) : (
+                      <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                    )}
+                  </Disclosure.Button>
                 </div>
               </div>
             </div>
